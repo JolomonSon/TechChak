9a6a8a6cc0b96844203a664def9ee611deae2696
John Solomon <jolomonson@gmail.com>
2023-01-22 20:29:53 +0000

    API: Implement weather data retrieval using OpenWeather API

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+import requests
 
 # Create your views here.
+def weather(request):
+  api_key = 'd17a1f5e7da5101c9409e9f5747b6c64'
+  location = ''
+
+  weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+
+  response = requests.get(weather_url)
+  data = response.json()
